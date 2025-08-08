f8c7b58f292c1c1c21a6256c11ef7e0b8e76d940
Sena Çetinkaya <67286519+sena-cetinkaya@users.noreply.github.com>
2025-08-08 20:56:50 +0300

    test_operations.py

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -9,7 +9,7 @@ class TestCalculator:
         assert operations.add(2, 3) == 5
 
     def test_add_fail(self):
-        assert operations.add(2, 3) == 6  # başarısız test
+        assert operations.add(2, 3) == 6  # fail test
 
     def test_subtract_success(self):
         assert operations.subtract(5, 3) == 2
@@ -32,3 +32,4 @@ class TestCalculator:
     def test_divide_by_zero(self):
         with pytest.raises(ValueError, match="Sıfıra bölme yapılamaz"):
             operations.divide(5, 0)
+
